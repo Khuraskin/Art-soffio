e7544706a880e1e55ab31b3d730285073de3a4c2
Khuraskin <88373364+Khuraskin@users.noreply.github.com>
2023-01-05 00:28:46 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,46 @@
+//hit slider
+const slider = new Swiper(".hit__wrapper", {
+  loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+//carousel swiper
+
+const swiper = new Swiper(".carousel__swiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 20,
+    },
+  },
+
+  loop: true,
+
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false,
+  },
+});
